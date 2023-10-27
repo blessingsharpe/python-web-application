c6b21ce3dbc0e1f2b7b737da6579b005822071fe
Nittylv <mcperuzi@hotmail.com>
2023-10-27 16:18:58 -0400

    new files

---
+++ docker-reg/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Set environment variables (if needed)
+#export MY_ENV_VAR=value
+
+# Perform any configuration tasks
+echo "Configuring the application..."
+
+# Start the main application process
+echo "Starting the application..."
+exec my-application-binary
+
+
+#docker build -t my-nginx:latest .
+#docker run -d -p 5001:5000 my-nginx:latest
+
